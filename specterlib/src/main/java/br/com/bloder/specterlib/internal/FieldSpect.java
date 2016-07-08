707cb6121ce6418e3a2b9084a416718ce3cd075d
Daniel <wowmegadenis@hotmail.com>
2016-07-08 02:23:58 -0300

    Add field spect annotation interface

---
+++ specterlib/src/main/java/br/com/bloder/specterlib/internal/FieldSpect.java
@@ -0,0 +1,17 @@
+package br.com.bloder.specterlib.internal;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Created by bloder on 07/07/16.
+ */
+
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface FieldSpect {
+
+  String name();
+}
