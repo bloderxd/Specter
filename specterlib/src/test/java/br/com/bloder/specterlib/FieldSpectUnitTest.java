136b4ed4b33275b4403c5b1bc55f22e0c1ed72a9
Daniel <wowmegadenis@hotmail.com>
2016-07-10 15:02:26 -0300

    Add correct tests

--- specterlib/src/test/java/br/com/bloder/specterlib/FieldSpectUnitTest.java
+++ specterlib/src/test/java/br/com/bloder/specterlib/FieldSpectUnitTest.java
@@ -2,8 +2,13 @@ package br.com.bloder.specterlib;
 
 import org.junit.Test;
 
+import br.com.bloder.specterlib.internal.Specter;
+import br.com.bloder.specterlib.test.SubPayload;
+import br.com.bloder.specterlib.test.SubPojo;
 import br.com.bloder.specterlib.test.TestPayload;
 import br.com.bloder.specterlib.test.TestPojo;
+import br.com.bloder.specterlib.test.ThirdPayload;
+import br.com.bloder.specterlib.test.ThirdPojo;
 
 import static org.junit.Assert.assertEquals;
 
@@ -12,18 +17,36 @@ import static org.junit.Assert.assertEquals;
  */
 public class FieldSpectUnitTest {
 
+  TestPojo testPojo = new TestPojo("", 0, 0, new SubPojo("", 0, new ThirdPojo("")));
+  TestPayload testPayload = new TestPayload("Daniel", 18, 12, new SubPayload("Bloder", 16, new ThirdPayload("Hello")));
+
   @Test
-  public void fieldSpectTransition() {
-
-    TestPayload testPayload = new TestPayload("Daniel", 18, 12);
-
-    TestPojo testPojo = new TestPojo("", 0, 0);
+  public void fieldSpectNormalTransition() {
     testPojo = new Specter<TestPojo, TestPayload>()
             .transform(testPayload)
             .in(testPojo)
             .withFieldContext();
 
     assertEquals("Daniel", testPojo.name);
-    assertEquals(18, testPojo.age);
+  }
+
+  @Test
+  public void fieldSpectWithSubClassTransition() {
+    testPojo = new Specter<TestPojo, TestPayload>()
+            .transform(testPayload)
+            .in(testPojo)
+            .withFieldContext();
+
+    assertEquals("Bloder", testPojo.subPojo.subName);
+  }
+
+  @Test
+  public void fieldSpectWithThirdClassTransition() {
+    testPojo = new Specter<TestPojo, TestPayload>()
+            .transform(testPayload)
+            .in(testPojo)
+            .withFieldContext();
+
+    assertEquals("Hello", testPojo.subPojo.exampleTestPayload.test);
   }
 }
