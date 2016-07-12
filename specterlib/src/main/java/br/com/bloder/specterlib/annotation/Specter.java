ed68ecda8e6d4a720967e39143743a3646a2210b
Daniel <wowmegadenis@hotmail.com>
2016-07-12 03:35:49 -0300

    Rename field spect to specter

--- specterlib/src/main/java/br/com/bloder/specterlib/annotation/Specter.java
+++ specterlib/src/main/java/br/com/bloder/specterlib/annotation/Specter.java
@@ -11,7 +11,7 @@ import java.lang.annotation.Target;
 
 @Target(ElementType.FIELD)
 @Retention(RetentionPolicy.RUNTIME)
-public @interface FieldSpect {
+public @interface Specter {
 
   String name();
 }
